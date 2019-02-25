7048561906bf57f95663e26024d294b69a0f8148
betht1220 <10212Parkview>
2019-02-25 13:11:56 -0800

    commit

--- tasks/cf-cli.sh
+++ tasks/cf-cli.sh
@@ -1,4 +1,17 @@
-#!/usr/bin/env bash
-set -e
-cd cf-env
-cf start $the-app
+#!/bin/bash
+cd code
+echo "music-album" >> app-names.txt
+IFS=$'\n' read -d '' -r -a allApps < app-names.txt
+
+cd ../cf_command
+
+COMMAND=start
+cat > params.yml <<EOF
+commands:
+EOF
+for i in "${allApps[@]}"
+do
+   :
+   echo "- command: " $COMMAND >> params.yml
+   echo "  app_name: " $i >> params.yml
+done
